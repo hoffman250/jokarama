03ba7586bca115e80ec9d91808885a2d66bc8b3e
hoffman250 <hoffman250@gmail.com>
2017-10-30 11:51:56 -0700

    Added SingleFragmentActivity.java

---
+++ Jokarama/app/src/main/java/brianhoffman/jokarama/SingleFragmentActivity.java
@@ -0,0 +1,11 @@
+package brianhoffman.jokarama;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by brianhoffman on 10/30/17.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    
+}
