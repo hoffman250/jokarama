ddaa659bab9dab83d826c937356d20f4e6675f6e
hoffman250 <hoffman250@gmail.com>
2017-10-30 17:11:08 -0700

    added recyclerview create layout manager to JokeListFragment.

--- Jokarama/app/src/main/java/brianhoffman/jokarama/JokeListFragment.java
+++ Jokarama/app/src/main/java/brianhoffman/jokarama/JokeListFragment.java
@@ -1,10 +1,29 @@
 package brianhoffman.jokarama;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by brianhoffman on 10/30/17.
  */
 
 public class JokeListFragment extends Fragment {
+
+    private RecyclerView mJokeRecyclerView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_joke_list, container, false);
+
+        mJokeRecyclerView = (RecyclerView) view.findViewById(R.id.joke_recycler_view);
+        mJokeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
+    }
 }
