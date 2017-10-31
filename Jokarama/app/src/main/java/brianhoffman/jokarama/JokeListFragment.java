2e636ae36400fa86f3eb2654108c418b361edf57
hoffman250 <hoffman250@gmail.com>
2017-10-30 17:33:39 -0700

    Added layout for viewholder, added code for jokeHolder and jokeAdapter in JokeListFragment

--- Jokarama/app/src/main/java/brianhoffman/jokarama/JokeListFragment.java
+++ Jokarama/app/src/main/java/brianhoffman/jokarama/JokeListFragment.java
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 /**
  * Created by brianhoffman on 10/30/17.
  */
@@ -15,6 +17,7 @@ import android.view.ViewGroup;
 public class JokeListFragment extends Fragment {
 
     private RecyclerView mJokeRecyclerView;
+    private JokeAdapter mAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,6 +27,47 @@ public class JokeListFragment extends Fragment {
         mJokeRecyclerView = (RecyclerView) view.findViewById(R.id.joke_recycler_view);
         mJokeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+        updateUI();
+
         return view;
+    }
+
+    private void updateUI() {
+        JokeRepo jokeRepo = JokeRepo.get(getActivity());
+        List<Joke> jokes = jokeRepo.getJokes();
+
+        mAdapter = new JokeAdapter(jokes);
+        mJokeRecyclerView.setAdapter(mAdapter);
+    }
+
+    private class JokeHolder extends RecyclerView.ViewHolder {
+        public JokeHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_joke, parent, false));
+        }
+    }
+
+    private class JokeAdapter extends RecyclerView.Adapter<JokeHolder> {
+        private List<Joke> mJokes;
+
+        public JokeAdapter(List<Joke> jokes) {
+            mJokes = jokes;
+        }
+
+        @Override
+        public JokeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+
+            return new JokeHolder(layoutInflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(JokeHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mJokes.size();
+        }
     }
 }
